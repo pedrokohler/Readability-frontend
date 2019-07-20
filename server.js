5a354647d8852cd5ea8c214d1d1168d23cb15ae5
Pedro Köhler <pedrokohlerbh@gmail.com>
2019-07-20 16:46:05 -0300

    Fix server path.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname)));
+app.use(express.static(path.resolve(__dirname, '/dist')));
 
 app.listen(process.env.PORT || 8080);
